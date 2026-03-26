b97c639ffaa1a88d18f01ec87083cf02ba055e3c
Jaryn2 <147291303+Jaryn2@users.noreply.github.com>
2026-03-26 11:41:36 -0400

    fixed player
    player didnt die to spikes right below when falling

--- CSMidTermGame/Player.java
+++ CSMidTermGame/Player.java
@@ -133,14 +133,17 @@ public class Player extends Actor
         {
             reset();
         }
-        Actor spikeCheck = getOneObjectAtOffset(getImage().getWidth() / 2 - 5, 0, Spike.class);
+        Actor spikeCheckRight = getOneObjectAtOffset(getImage().getWidth() / 2 - 5, 0, Spike.class);
+        Actor spikeCheckLeft = getOneObjectAtOffset(getImage().getWidth() / 2 * -1 + 5, 0, Spike.class);
         Actor blockInFrontCheckMiddleRight = getOneObjectAtOffset(getImage().getWidth() / 2 - 5, 10, MiddleBlock.class);
         Actor blockInFrontCheckPermRight = getOneObjectAtOffset(getImage().getWidth() / 2 - 5, 10, Block.class);
         Actor blockInFrontCheckMiddleLeft = getOneObjectAtOffset(getImage().getWidth() / 2 * -1 + 5, 10, MiddleBlock.class);
         Actor blockInFrontCheckPermLeft = getOneObjectAtOffset(getImage().getWidth() / 2 * -1 + 5, 10, Block.class);
 
 
-        if (spikeCheck != null || blockInFrontCheckMiddleLeft != null || blockInFrontCheckPermLeft != null || blockInFrontCheckMiddleRight != null || blockInFrontCheckPermRight != null)
+        if (spikeCheckRight != null || spikeCheckLeft != null 
+        || blockInFrontCheckMiddleLeft != null || blockInFrontCheckPermLeft != null 
+        || blockInFrontCheckMiddleRight != null || blockInFrontCheckPermRight != null)
         {
             reset();
         }
